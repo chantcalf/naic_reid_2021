410ead016de4e69d281e7cfc202aa006480261c5
wqh17101 <597935261@qq.com>
2022-01-20 00:09:14 +0800

    init

--- rerank_task/predict.py
+++ rerank_task/predict.py
@@ -4,10 +4,14 @@ Created on Wed Jan 19 19:12:47 2022
 
 @author: chantcalf
 """
-import os
 import json
+import os
+
 import numpy as np
 import torch
+from tqdm import tqdm
+
+work_dir = os.path.realpath(os.path.dirname(__file__))
 os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -23,17 +27,17 @@ class TestDataSet:
         self.fpaths = [os.path.join(dat_dir, i) for i in names]
         self.names = [i.replace('.dat', '.png') for i in names]
         self.num = len(self.names)
-        
+
     def __len__(self):
         return self.num
-    
+
     def __getitem__(self, index):
         return index, read_dat(self.fpaths[index])
 
 
 class DefaultCfg:
-    query_path = './test_A/testA_query_features'
-    gallery_path = './test_A/testA_gallery_features'
+    query_path = os.path.join(work_dir, '../data/test_A/query_feature_A')
+    gallery_path = os.path.join(work_dir, '../data/test_A/gallery_feature_A')
     query_batch_size = 200
     gallery_batch_size = 1000
     num_workers = 0
@@ -48,18 +52,18 @@ def get_norm(x):
 def get_cosine_dist(x, y):
     x = get_norm(x)
     y = get_norm(y)
-    return x @ y.transponse(0, 1)
-    
-    
+    return x @ y.transpose(0, 1)
+
+
 def predict_res(cfg, query_loader, gallery_loader):
     res = []
-    for q_id, q_data in query_loader:
+    for q_id, q_data in tqdm(query_loader):
         q_data = q_data.to(device)
         best_dist = None
         best_id = None
-        for g_id, g_data in gallery_loader:
+        for g_id, g_data in tqdm(gallery_loader):
             g_data = g_data.to(device)
-            g_id = g_id.to(device).unsqueeze(0)
+            g_id = g_id.to(device).unsqueeze(0).expand(q_data.shape[0], -1)
             dist = get_cosine_dist(q_data, g_data)
             if best_dist is None:
                 best_dist = dist
@@ -68,10 +72,11 @@ def predict_res(cfg, query_loader, gallery_loader):
                 best_dist = torch.cat([best_dist, dist], -1)
                 best_id = torch.cat([best_id, g_id], -1)
             if best_dist.shape[1] > cfg.topk:
-                best_dist, tid = torch.topk(best_dist, dim=1)
+                best_dist, tid = torch.topk(best_dist, cfg.topk, dim=1)
                 best_id = best_id.gather(1, tid)
         res.append([q_id, best_id, best_dist])
     return res
+
 
 def main():
     cfg = DefaultCfg()
@@ -83,10 +88,10 @@ def main():
     gallery_loader = torch.utils.data.DataLoader(
         gallery, batch_size=cfg.gallery_batch_size, shuffle=False,
         num_workers=cfg.num_workers, pin_memory=True)
-    
+    print("start to predict")
     with torch.no_grad():
         res = predict_res(cfg, query_loader, gallery_loader)
-    
+    print("end predict")
     ans = dict()
     for i in range(len(res)):
         q_id = res[i][0]
@@ -95,10 +100,10 @@ def main():
         for j in range(b):
             j_name = query.names[q_id[j]]
             ans[j_name] = [gallery.names[g_id[j][k]] for k in range(n)]
-    
+
     with open(cfg.save_path, "w", encoding='utf8') as f:
         json.dump(ans, f)
-    
+
 
 if __name__ == "__main__":
     main()
