c955a8cf740891d931c4fbea8d261447bdf683e7
Chang Liu <magicallc@126.com>
2022-02-20 12:41:29 +0800

    Add files via upload
    使用ema

--- compress_task/train_compress.py
+++ compress_task/train_compress.py
@@ -37,6 +37,40 @@ def get_non_zero(ds):
     return [i for i in range(2048) if res[i] > 1e-5]
 
 
+class EMA:
+    def __init__(self, model, decay):
+        self.model = model
+        self.decay = decay
+        self.shadow = {}
+        self.backup = {}
+
+    def register(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                self.shadow[name] = param.data.clone()
+
+    def update(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                assert name in self.shadow
+                new_average = (1.0 - self.decay) * param.data + self.decay * self.shadow[name]
+                self.shadow[name] = new_average.clone()
+
+    def apply_shadow(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                assert name in self.shadow
+                self.backup[name] = param.data
+                param.data = self.shadow[name]
+
+    def restore(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                assert name in self.backup
+                param.data = self.backup[name]
+        self.backup = {}
+
+
 class DataSet:
     def __init__(self, fpaths):
         self.fpaths = fpaths
@@ -60,6 +94,27 @@ def set_seed(seed):
     torch.backends.cudnn.deterministic = True
 
 
+def validate(model, best_loss, test_loader, test_ds, cfg):
+        model.eval()
+        tloss = [0, 0, 0]
+        with torch.no_grad():
+            for i, x in enumerate(test_loader):
+                x = x.to(device)
+                for j, bit in enumerate(cfg.bs):
+                    y = model.encoder(x, bit)
+                    y = model.decoder(y, bit)
+                    tloss[j] = tloss[j] + (y - x).pow(2).sum(-1).sqrt().sum().item()
+            tloss = [i / len(test_ds) for i in tloss]
+        average_loss = sum(tloss)
+        lossp = ", ".join([f"{i:.3f}" for i in tloss])
+        LOGGER.info(f"average_loss={average_loss:.3f}, best_loss={best_loss:.3f}, " + lossp)
+        if average_loss < best_loss:
+            torch.save(model.state_dict(), cfg.model_path)
+            LOGGER.info("Model saved")
+            best_loss = average_loss
+        return best_loss
+
+
 def lr_scheduler(step, warm_up_step, max_step):
     if step < warm_up_step:
         return 1e-2 + (1 - 1e-2) * step / warm_up_step
@@ -69,19 +124,20 @@ def lr_scheduler(step, warm_up_step, max_step):
 class DefaultCfg:
     all_data = [
         os.path.join(TRAIN_DATA_DIR, 'train/train_feature'),
-        os.path.join(TRAIN_DATA_DIR, 'test_A/query_feature_A'),
-        os.path.join(TRAIN_DATA_DIR, 'test_A/gallery_feature_A')
+        # os.path.join(TRAIN_DATA_DIR, 'test_A/query_feature_A'),
+        # os.path.join(TRAIN_DATA_DIR, 'test_A/gallery_feature_A')
     ]
     seed = 1992
     batch_size = 256
-    epochs = 100
+    epochs = 200
     warmup = 1000
-    learning_rate = 6e-4
+    learning_rate = 1e-3
     weight_decay = 0.05
     train_sp = 0.9
     num_workers = NUM_WORKERS
     bs = [64, 128, 256]
     model_path = './compress.pth'
+    ema_decay = 0.999
 
 
 def main():
@@ -115,7 +171,8 @@ def main():
 
     model = TrainModel(cfg.bs, select_index)
     model.to(device)
-
+    ema = EMA(model, cfg.ema_decay)
+    ema.register()
     steps_per_epoch = len(train_ds) // cfg.batch_size
     optimizer = torch.optim.AdamW(model.parameters(),
                                   lr=cfg.learning_rate,
@@ -125,6 +182,7 @@ def main():
                          lr_lambda=partial(lr_scheduler,
                                            warm_up_step=cfg.warmup,
                                            max_step=max_step))
+    
     best_loss = np.inf
     for epoch in range(cfg.epochs):
         LOGGER.info("###########")
@@ -140,27 +198,15 @@ def main():
             clip_grad_norm_(model.parameters(), 10.)
             optimizer.step()
             scheduler.step()
+            ema.update()
             if i % 100 == 0:
                 loss_print = "\t".join([f"{key} {losses[key].item():.3f}" for key in losses])
                 LOGGER.info(f'Epoch: [{epoch}] step: [{i}/{steps_per_epoch}]\t' + loss_print)
 
-        model.eval()
-        tloss = [0, 0, 0]
-        with torch.no_grad():
-            for i, x in enumerate(test_loader):
-                x = x.to(device)
-                for j, bit in enumerate(cfg.bs):
-                    y = model.encoder(x, bit)
-                    y = model.decoder(y, bit)
-                    tloss[j] = tloss[j] + (y - x).pow(2).sum(-1).sqrt().sum().item()
-            tloss = [i / len(test_ds) for i in tloss]
-        average_loss = sum(tloss)
-        lossp = ", ".join([f"{i:.3f}" for i in tloss])
-        LOGGER.info(f"average_loss={average_loss:.3f}, best_loss={best_loss:.3f}, " + lossp)
-        if average_loss < best_loss:
-            torch.save(model.state_dict(), cfg.model_path)
-            LOGGER.info("Model saved")
-            best_loss = average_loss
+        best_loss = validate(model, best_loss, test_loader, test_ds, cfg)
+        ema.apply_shadow()
+        best_loss = validate(model, best_loss, test_loader, test_ds, cfg)
+        ema.restore()
 
         LOGGER.info(f"cost {time.time() - epoch_start_time}s")
         LOGGER.info("###########")
